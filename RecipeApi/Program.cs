e729a008047f47c0ebe73904f2f6dc1344ae9cf3
David Pine <david.pine@microsoft.com>
2021-02-09 12:28:56 -0600

    consider showing top-level statements

--- RecipeApi/Program.cs
+++ RecipeApi/Program.cs
@@ -1,16 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
+using RecipeApi;
 
-namespace RecipeApi
-{
-    class Program
-    {
-        static Task Main(string[] args) => 
-            CreateHostBuilder(args).Build().RunAsync();
-
-        static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
-    }
-}
+await Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+    .Build()
+    .RunAsync();
